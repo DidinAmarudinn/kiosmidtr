d51e081074e19843b9959291bd2a36203bf837de
didin <amarudin53@gmail.com>
2020-06-15 19:43:05 +0700

    update again

--- index.php
+++ index.php
@@ -18,4 +18,31 @@ if($SERVER['REQUEST_METHOD'] !== 'POST'){
 $request_body=file_get_contents('php://input');
 header('Content-Type: application/json');
 
+$charge_result=chargeAPI($api_url, $server_key, $request_body);
+http_response_code($charge_result['http_code']);
+
+echo $charge_result['body'];
+
+function chargeAPI($api_url, $server_key, $request_body){
+    $ch=curl_init();
+    $curl_options=array(
+        CURLOPT_URL => $api_url,
+        CURLOPT_RETURNTRANSFER => 1,
+        CURLOPT_POST => 1,
+        CURLOPT_HEADER => 0,
+
+        CURLOPT_HTTPHEADER => array(
+            'Accept: application/json',
+            'Content-Type: application/json',
+            'Authorization: Basic '. base64_encode($server_key . ':')
+        ),
+        CURLOPT_POSTFIELDS => $request_body
+    );
+    curl_setopt_array($ch, $curl_options);
+    $result= array(
+        'body' => curl_exec($ch),
+        'http_code' => curl_getinfo($ch, CURLINFO_HTTP_CODE),
+    );
+    return $result;
+}
 ?>
